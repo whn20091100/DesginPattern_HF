32921afc16cbbae8753bb2a5d56210be8e434f97
whn20091100 <whn20091100@163.com>
2017-08-07 14:51:07 +0800

    Desgin Pattern HF book init

--- src/ObserverPattern/headfirst/observer/WeatherStation.java
+++ src/ObserverPattern/headfirst/observer/WeatherStation.java
@@ -6,9 +6,17 @@ public class WeatherStation {
 
         CurrentConditionsDisplay currentConditionsDisplay =
                 new CurrentConditionsDisplay(weatherData);
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
 
+
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         weatherData.setMeasurements(80, 65, 30.4f);
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         weatherData.setMeasurements(82, 70, 29.2f);
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         weatherData.setMeasurements(78, 90, 29.2f);
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
     }
 }
